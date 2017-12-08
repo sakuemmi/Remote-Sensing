f594b89558f3947e4bbcbd408a8108f06150f323
Berat Karabacak <karabacakberat26@hotmail.com>
2017-12-08 14:46:48 +0300

    Update and rename Missing Line Kodu.py to Missing Line.py

--- Missing Line.py
+++ Missing Line.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-goruntu = cv2.imread('irvine_NIR.tif')
+goruntu = cv2.imread('image')
 plt.hist(goruntu.ravel(),256,[0,256]); plt.show()
  
 for  k in range(0,512):
 	for j in range(0,512):
 		if goruntu[k][j][...,0] == 0 and goruntu[k][j][...,1] == 0 and goruntu [k][j][...,2] == 0:
 			goruntu[k][j] = (goruntu[k+1][j] / 2) + (goruntu[k-1][j] / 2)
-cv2.imshow('duzenlenmis_goruntu' ,goruntu)
+cv2.imshow('result_image' ,goruntu)
 cv2.waitKey(0)
 			
 
